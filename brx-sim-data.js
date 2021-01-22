3bdf8829fb39790672e716b3a41842abe957dd1e
Rich Norris <rnorris@automationdirect.com>
2021-01-22 09:35:21 -0500

    Add parameter handling for simulating the data/json BRX endpoint, supports mulitple query parameters and selecting slices for array values in the sim data. Adds separate js module to store all the canned sim data, so don't have to touch code file, Update copyright to ADC

---
+++ brx-sim-data.js
@@ -0,0 +1,19 @@
+
+let simData = {
+    "SDFileNames":["FontAwesome.otf","fontawesome-webfont.eot","fontawesome-webfont.svg","fontawesome-webfont.ttf","fontawesome-webfont.woff","fontawesome-webfont.woff2","MyFILELOG_y2020m12d23.csv","MyFILELOG_y2020m12d24.csv","MyFILELOG_y2019m10d11.csv","BRX-DataLog-Chartist-Legend.html","","","","","","","","","","","","","","",""],
+    "ProcessTank1":{"level":15.3,"enabled":0,"ll_alm_sp":0,"l_alm_sp":0,"h_alm_sp":20,"hh_alm_sp":25,"hh_alm":0,"h_alm":0,"ll_alm":0,"l_alm":0,"is_eff_open":0,"is_infl_open":0,"alm_count":0},
+    "InfeedConveyor":{"actualSPD":98,"commandSPD":100,"running":1,"stopped":0,"enabled":0,"faulted":0},
+    "Conveyor1":{"actualSPD":15.3,"commandSPD":0,"running":0,"stopped":0,"enabled":0,"faulted":0},
+    "MyTimer":{"Acc": 0,"Done": 0},
+    "TIMER2":{"Acc": 123,"Done": 1},
+    "MyDeeEssTeeZero": 104883274,
+    "FirstTenDs":[110137182,825,575,11745,794,0,0,0,0,0],
+    "Linear":89206.5,
+    "Cosine":-59.8053,
+    "Random":92.1708,
+    "SineRandom":5.5991,
+    "MyMain":{"RanThisScan":1,"Done":0,"FirstRun":0,"TimeSlice":65535,"ScanCounter":37579023,"RunCounter":0,"S0_15":0,"S16_31":0,"S32_47":0,"S48_63":0,"S64_79":0,"S80_95":0,"S96_111":0,"S112_127":0},
+    "EmptyPlaceholder":{}
+};
+
+export default simData;
--- index.js
+++ index.js
@@ -1,34 +1,107 @@
 //import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { get } from 'http';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import BRX_SIM_DATA from './brx-sim-data.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
  
 const app = express();
 app.use(cors());
 
-let WEB_ROOT = path.join(__dirname + '/public');
+let WEB_ROOT = path.join(__dirname, 'public');
 console.log(`Using ${WEB_ROOT} as ROOT web dir`);
 app.use(express.static(WEB_ROOT));
  
+let NOT_FOUND_MSG = '<!DOCTYPE HTML>\n<html>\n<body>\n<h1>Not Found</h1>\n</body>\n</html>';
+let BAD_REQUEST_MSG = '<!DOCTYPE HTML>\n<html>\n<body>\n<h1>Bad Request</h1>\n</body>\n</html>';
+
 app.get('/', (req, res) => {
     res.send('Hello BRX Sim! <br>You are successfully running the web development simulator!');
-  });
-
-
-// Request for Array of Strings
-//data/json?SDFileNames=SDFileNames0,25
-// Example JSON: 
-
-//
-app.get('/data/json', (req, res) => {
-  res.send('{"SDFileNames":["FontAwesome.otf","fontawesome-webfont.eot","fontawesome-webfont.svg","fontawesome-webfont.ttf","fontawesome-webfont.woff","fontawesome-webfont.woff2","MyFILELOG_y2020m12d28.csv","MyFILELOG_y2020m12d16.csv","MyFILELOG_y2019m10d11.csv","BRX-DataLog-Chartist-Legend.html","","","","","","","","","","","","","","",""]}');
 });
 
+
+// Simulate Data Request API of BRX PLC
+app.get('/data/json', (req, res) => {
+  
+  if (isEmptyQuery(req.query, res)) return;
+
+  logDataRequestParameters(req);
+
+  let results = new Object;
+  // Collect results for each parameter in request
+  for (const paramName in req.query) {
+    console.log("Current paramName = ", paramName);
+    // handle any bad request parameters
+    if (!isValidDataJsonParameter(paramName, res)) return;
+
+    results[paramName] = getResultsForParameter(paramName, req.query[paramName]);
+  }
+  res.send(results);
+
+});
+
+
+function getResultsForParameter(name, value) {
+  console.log("Processing parameter", name, "with value", value);
+  let results = BRX_SIM_DATA[name];
+  // Handle if this is an Array and the request included a valid number of items, given the Sim data we have
+  let count = getCountFromValue(value);
+  console.log(Array.isArray(results), count);
+  if (Array.isArray(results) && count > 0) {
+      if (count < results.length) {
+        return results.slice(0,count);
+      }
+  } 
+  return results;
+}
+
+// Parse out the count value after comma, from param value like: data/json?FirstTenDs=DST0,10
+// Should return 10
+function getCountFromValue(value) {
+  if (! value.includes(",")) return 0;
+  let valueParts = value.split(",",2);
+  return parseInt(valueParts[1]);
+}
+
+function logDataRequestParameters(req) {
+  for (const key in req.query) {
+    console.log("Called /data/json with parameter:",key, req.query[key]);
+  }
+  let paramNames = Object.keys(req.query);
+  console.log("Parameter Names", paramNames.join());
+
+}
+
+function isEmptyQuery(query, res) {
+  let paramNames = Object.keys(query);
+  if (paramNames.length == 0) {
+    // Return 404
+    console.log("data/json Request with no query parameter given. Returning 404-Not Found")
+    res.status(404).send(NOT_FOUND_MSG); 
+    return true;
+  }
+  return false;
+}
+
+function isValidDataJsonParameter(paramName, res) {
+  // Return 400 for a bad parameterName
+  if (!BRX_SIM_DATA[paramName]) {
+    console.log(`data/json Request with unknown parameter ${paramName}. Possibly need addition to brx-sim-data.js. Returning 400-Bad Request`)
+    res.status(400).send(BAD_REQUEST_MSG); 
+    return false;
+  }
+  return true;
+}
 
 //  app.listen(process.env.PORT, () =>
   app.listen(3000, () =>
   console.log('BRX-SIM Example app listening on port 3000!'),
 );
+
+
+// Valid PLC Resources for data/json requests
+// Timers: T0,L through T256,L
+//
