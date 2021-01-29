158d3711d8510519dc7ba07004880da77712158f
Rogers <arogers@automationdirect.com>
2021-01-29 16:44:40 -0500

    rename myFunction to toggleMenu for readability, move inline style for hamburger to css

--- public/sd/web/js/example-chartist-directory.js
+++ public/sd/web/js/example-chartist-directory.js
@@ -1,6 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+function toggleMenu() {
+    var x = document.getElementById("topnav");
+    if ( x.className === "topnav" ) {
         x.className += " responsive";
     } else {
         x.className = "topnav";
--- public/sd/web/js/example-linear-gauge.js
+++ public/sd/web/js/example-linear-gauge.js
@@ -1,9 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
+function toggleMenu() {
+    var x = document.getElementById("topnav");
     if ( x.className === "topnav" ) {
         x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
 }
 
